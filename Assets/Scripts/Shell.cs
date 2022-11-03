33b03f796fdd306b84a01f09e2dbe02e0c667c55
SoulFrequence <goncalossantos11@gmail.com>
2022-11-03 21:30:36 +0000

    a47513 - 04 Trajectories Part 1 & 2

--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class Shell : MonoBehaviour
 {
     public GameObject explosion;
+
+    Rigidbody rb;
+    /*
     float mass = 10;
     float force = 500;
     float accel;
     float speedZ;
     float speedY;
     float Gravity = -9.8f;
-    float gAccel;
+    float gAccel;*/
 
     void OnCollisionEnter(Collision col)
     {
@@ -26,23 +29,22 @@ public class Shell : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
+
+        this.transform.forward = rb.velocity;
+
+        /*
         accel = force/mass;
-
         speedZ += accel * Time.deltaTime;
-
         gAccel = Gravity / mass;
-
         speedY += gAccel * Time.deltaTime;
-
         this.transform.Translate(0, speedY, speedZ);
-
         force = 0;
-
+        */
     }
 }
--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -7,6 +7,11 @@ public class Shoot : MonoBehaviour
 
     public GameObject shellPrefab;
     public GameObject shellSpawnPos;
+    public GameObject target;
+    public GameObject parent;
+    float speed = 15;
+    float turnSpeed = 2;
+    bool CanShoot = true;
 
     // Start is called before the first frame update
     void Start()
@@ -14,16 +19,98 @@ public class Shoot : MonoBehaviour
         
     }
 
+    void CanShootAgain(){
+
+        CanShoot = true;
+        
+    }
+
     void Fire(){
 
-        GameObject shell = Instantiate(shellPrefab, shellSpawnPos.transform.position, shellSpawnPos.transform.rotation);
+        if(CanShoot){
+
+            GameObject shell = Instantiate(shellPrefab, shellSpawnPos.transform.position, shellSpawnPos.transform.rotation);
+
+            shell.GetComponent<Rigidbody>().velocity = speed * this.transform.forward;
+
+            CanShoot = false;
+
+            Invoke("CanShootAgain", 0.25f);
+        }
+
+        
 
     }
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
-            Fire(); 
+        
+            Vector3 direction = (target.transform.position - parent.transform.position).normalized;
+
+            Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+
+            parent.transform.rotation = Quaternion.Slerp(parent.transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+
+            float? angle = RotateTurret();
+
+            
+            if(angle != null && Vector3.Angle(direction, parent.transform.forward) < 10){
+
+                Fire(); 
+
+            }
+            
+    }
+
+    float? RotateTurret(){
+
+        float? angle = CalculateAngle(false);
+
+        if(angle != null){
+
+            this.transform.localEulerAngles = new Vector3(360f - (float) angle, 0f, 0f);
+        } 
+        return angle;
+    }
+
+    float? CalculateAngle(bool low){
+
+        Vector3 targetDir = target.transform.position - this.transform.position;
+
+        float y = targetDir.y;
+
+        targetDir.y = 0f;
+
+        float x = targetDir.magnitude;
+
+        float gravity = 9.81f;
+
+        float sSqr = speed * speed;
+
+        float underTheSqrRoot = (sSqr * sSqr) - gravity * (gravity * x * x + 2 * y * sSqr);
+
+        if(underTheSqrRoot >= 0f){
+
+            float root = Mathf.Sqrt(underTheSqrRoot);
+
+            float highAngle = sSqr + root;
+
+            float lowAngle = sSqr - root;
+
+            if(low){
+
+                return(Mathf.Atan2(lowAngle, gravity * x) * Mathf.Rad2Deg);
+
+            } else {
+
+                return(Mathf.Atan2(highAngle, gravity * x) * Mathf.Rad2Deg);
+
+            }
+        } else {
+
+            return null;
+
         }
     }
+
 }
