aa5a65ab8a7fd764781065993a319c7ec14d9d7a
SoulFrequence <goncalossantos11@gmail.com>
2022-12-04 18:37:48 +0000

    a47513 - 02 Crowd Simulation (Part 1)

---
+++ Assets/AIControl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class AIControl : MonoBehaviour {
+
+    GameObject[] goalLocations;
+    UnityEngine.AI.NavMeshAgent agent;
+    Animator animation;
+
+
+    // Use this for initialization
+    void Start() {
+        goalLocations = GameObject.FindGameObjectsWithTag("goal");
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+        animation = this.GetComponent<Animator>();
+        animation.SetTrigger("isWalking");
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (agent.remainingDistance < 1) {
+
+            agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+        }
+    }
+}
