60595a77fde0af92a5ebdae0f9d76a4d4c9bd1fa
SoulFrequence <goncalossantos11@gmail.com>
2022-12-06 12:48:14 +0000

    a47513 - 03 Machine Learning AI Done

--- Assets/ML-Agents/Examples/Basic/Scripts/MoveToGoal.cs
+++ Assets/ML-Agents/Examples/Basic/Scripts/MoveToGoal.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Sensors;
+using Unity.MLAgents.Actuators;
+
 
 public class MoveToGoal : Agent
 {
@@ -13,7 +15,7 @@ public class MoveToGoal : Agent
     public override void OnEpisodeBegin()
     {
 
-        transform.localPosition = new Vector3(Random.Range(-7.5f, +3.8f), 0, Random.Range(-2.6f, +2.3f));
+        transform.localPosition = new Vector3(Random.Range(-7.5f, +3.8f), 1.09f, Random.Range(-2.6f, +2.3f));
 
         targetTransform.localPosition = new Vector3(Random.Range(4.9f, +7.7f), 0, Random.Range(-2.7f, +1.6f));
     }
@@ -23,20 +25,6 @@ public class MoveToGoal : Agent
         sensor.AddObservation(transform.localPosition);
         sensor.AddObservation(targetTransform.localPosition);
     }
-
-  /* Código Sugerido pelo auto complete (Tem Erros)
-    public override void OnActionReceived(float[] actions)
-    {
-         float moveX = actions.ContinuousActions[0];
-
-         float moveZ = actions.ContinuousActions[1];
-
-         float moveSpeed = 1f;
-
-         transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
-    }
-
-    Código Feito no tutorial aparece com erros aqui apesar de estar bem feito
      
     public override void OnActionReceived(ActionBuffers actions)
     {
@@ -44,7 +32,7 @@ public class MoveToGoal : Agent
 
         float moveZ = actions.ContinuousActions[1];
 
-        float moveSpeed = 1f;
+        float moveSpeed = 3f;
 
         transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
     }
@@ -58,15 +46,16 @@ public class MoveToGoal : Agent
 
         continuousActions[1] = Input.GetAxisRaw("Vertical");
 
-    }*/
+    }
+  
 
     private void OnTriggerEnter(Collider other)
     {
-
+        Debug.Log("Geral");
         if(other.TryGetComponent<Goal>(out Goal goal))
         {
-
-            SetReward(1f);
+            Debug.Log("Goal");
+            SetReward(+1f);
             floorMeshRenderer.material = winMaterial;
             EndEpisode();
 
@@ -74,6 +63,7 @@ public class MoveToGoal : Agent
 
         if (other.TryGetComponent<Walls>(out Walls wall))
         {
+            Debug.Log("Wall");
             SetReward(-1f);
             floorMeshRenderer.material = loseMaterial;
             EndEpisode();
